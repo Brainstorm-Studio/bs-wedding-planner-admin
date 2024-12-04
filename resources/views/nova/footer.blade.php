73a825a7dbb6e64d8662d2acf237b5ea75cbac8c
roberto <roberto.navarretem@gmail.com>
2024-12-04 16:49:13 -0500

    install base

---
+++ resources/views/nova/footer.blade.php
@@ -0,0 +1,3 @@
+<div class="mt-8 leading-normal text-xs text-gray-500 space-y-1"><p class="text-center">Powered by <a class="link-default" href="https://brainstormstudio.dev">Brainstorm Studio</a></p>
+    {{-- <p class="text-center">© 2023 Laravel LLC · by Taylor Otwell and David Hemphill.</p> --}}
+</div>
