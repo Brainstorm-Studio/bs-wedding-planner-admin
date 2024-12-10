bdebe5d6a8f3956a8f1a98bd919a49f72cd1db40
roberto <roberto.navarretem@gmail.com>
2024-12-09 19:18:34 -0500

    add countries apis

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Country;
 use Illuminate\Http\Request;
 
 class CountryController extends Controller
 {
-    //
+    public function getCountries(){
+        try {
+            $countries = Country::get()
+                ->map(function ($country) {
+                    return [
+                        'id' => $country->id,
+                        'name' => $country->name,
+                        'code' => $country->code,
+                        'phone_code' => $country->phone_code,
+                    ];
+                });
+
+            return [
+                'data' => $countries,
+                'message' => 'Countries fetched successfully'
+            ];
+
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage()], 500);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GuestController;
+use App\Http\Controllers\CountryController;
 use App\Http\Controllers\WeddingController;
 
 Route::get('/wedding/{slug}', [WeddingController::class, 'getWeddingBySlug']);
 Route::get('/guests/{slug}', [GuestController::class, 'getGuestsByWedding']);
 Route::get('/search-guest/{slug}/{phone}', [GuestController::class, 'getGuestByWeddingAndPhone']);
 Route::post('/update-guest', [GuestController::class, 'updateGuest']);
+Route::get('/countries', [CountryController::class, 'getCountries']);
